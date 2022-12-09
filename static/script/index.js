4dd540753a59baba9a85ab9938158c6afa1a97ae
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-12-09 15:38:18 +0900

    Update index.js
    `index.js`는 다른 모듈에서 가져온 함수를 실질적으로 실행하는 역할로 바꾸었습니다.

--- static/script/index.js
+++ static/script/index.js
@@ -1,3 +1,11 @@
 "use strict";
+// @ts-check
 
+import showBody from "./elements.js";
+import addEventListenerToButtons from "./buttons.js";
+import {EVENT} from "./literals.js";
 
+window.addEventListener(EVENT.load, () => {
+    showBody();
+    addEventListenerToButtons();
+});
